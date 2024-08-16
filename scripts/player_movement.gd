43f535a00b30e89f868ba3c9e9ff66951a2db429
Jonas Kuball <jonas.kuball@lector.ai>
2024-08-17 00:06:56 +0200

    also scale movement

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -6,8 +6,7 @@ const Bocks: PackedScene = preload("res://scenes/bocks.tscn")
 
 signal splittle(scale)
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 58000.0
 
 var bocksScale = 1.0
 var spawn_right = true
@@ -40,12 +39,9 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("player_left", "player_right")
 	if direction:
-		if direction>0:
-			spawn_right = true
-		else:
-			spawn_right = false	
-		velocity.x = direction * SPEED
+		spawn_right = direction > 0
+		velocity.x = direction * SPEED * bocksScale * delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED * bocksScale * delta)
 
 	move_and_slide()
