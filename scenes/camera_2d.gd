9d66d0ac20c7979b5c9a027888d643c6c8e6a9b2
FloPeace <flo.spacemungos@gmail.com>
2024-08-16 22:38:46 +0200

    Camera Zoom

---
+++ scenes/camera_2d.gd
@@ -0,0 +1,18 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_player_splittle(scale: Vector2) -> void:
+	pass # Replace with function body.
+	set_zoom(get_zoom()*scale)
+
+	
--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 
 const Bocks: PackedScene = preload("res://scenes/bocks.tscn")
 
+signal splittle(scale)
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -17,6 +19,9 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("player_splittle"):
 		var bocks = Bocks.instantiate()
 		spawn_target.add_child(bocks)
+		apply_scale(Vector2(0.5,0.5))
+		splittle.emit(Vector2(2, 2))
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
