40a99ff78c2c178cde320c0d7e5a562b45fdc5d3
FloPeace <flo.spacemungos@gmail.com>
2024-08-17 23:44:48 +0200

    Load next level on goal

--- scripts/goal.gd
+++ scripts/goal.gd
@@ -1,8 +1,17 @@
 extends Node2D
 
-signal goal_reached
+
+const FILE_BEGIN = "res://scenes/levels/level"
+const FILE_TYPE = ".tscn"
 
 func _on_area_2d_body_entered(body):
 	# if player is body, then send "goal reached"
+	#if body.is_in_group("Player"):
 	if body.name == "Player":
-		goal_reached.emit()
+		var current_scene_file = get_tree().current_scene.scene_file_path
+		var next_level_number = current_scene_file.to_int() + 1
+		#print_debug(next_level_number)
+		
+		var next_level_path = FILE_BEGIN+str(next_level_number)+FILE_TYPE
+		#print_debug(next_level_path)
+		get_tree().change_scene_to_file(next_level_path)
