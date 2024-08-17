4ebab00aa8dae752d8d32dfc78b1b0baf3491b9d
FloPeace <flo.spacemungos@gmail.com>
2024-08-17 13:12:34 +0200

    Scale

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -4,16 +4,18 @@ extends CharacterBody2D
 
 const Bocks: PackedScene = preload("res://scenes/bocks.tscn")
 
-signal splittle(scale)
+signal splittle(scale : Vector2)
 
 const SPEED = 58000.0
 
 var bocksScale = 1.0
-var spawn_right = true
+var look_right = true
+
+var is_scale_bocks = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
+	if (not is_on_floor()) and (not is_scale_bocks):
 		velocity += get_gravity() * delta
 
 	# Handle jump.
@@ -25,7 +27,7 @@ func _physics_process(delta: float) -> void:
 		bocks.get_child(0).apply_scale(Vector2(bocksScale,bocksScale))
 		bocksScale = bocksScale/2
 		print_debug(get_child(0).texture.get_size().x*scale.x)
-		if spawn_right:
+		if look_right:
 			bocks.set_global_position(get_global_position()+Vector2(get_child(0).texture.get_size().x*scale.x,0))
 		else:
 			bocks.set_global_position(get_global_position()+Vector2(-get_child(0).texture.get_size().x*scale.x,0))
@@ -39,9 +41,33 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("player_left", "player_right")
 	if direction:
-		spawn_right = direction > 0
-		velocity.x = direction * SPEED * bocksScale * delta
+		look_right = direction > 0
+		if is_scale_bocks:
+			if look_right:
+				velocity.y = -direction * SPEED * bocksScale * delta
+			else:
+				velocity.y = direction * SPEED * bocksScale * delta
+		else:
+			velocity.x = direction * SPEED * bocksScale * delta
 	else:
+		if is_scale_bocks:
+			velocity.y = move_toward(velocity.y, 0, SPEED * bocksScale * delta)
 		velocity.x = move_toward(velocity.x, 0, SPEED * bocksScale * delta)
 
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Bocks"):
+		print_debug("InGroup")
+		if not is_scale_bocks:
+			is_scale_bocks = true
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Bocks"):
+		print_debug("OutGroup")
+		if is_scale_bocks:
+			is_scale_bocks = false
+	pass # Replace with function body.
